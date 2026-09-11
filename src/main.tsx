c327a1004b12ca48b5c8d17d668cd95972240703
michelzaum <michel.oliveira999@hotmail.com>
2026-09-10 21:24:08 -0300

    feat: create routes.tsx file with route for Dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 
 import './index.css'
-import App from './App.tsx'
+import { RoutesComponent } from './routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <RoutesComponent />
     </BrowserRouter>,
   </StrictMode>,
 )
