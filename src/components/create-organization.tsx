5d70da02ba04e3fa0f3ce1a45c6385eecb8433e5
michelzaum <michel.oliveira999@hotmail.com>
2026-09-12 21:50:15 -0300

    feat: integrate 'create organization' endpoint with backend

--- src/components/create-organization.tsx
+++ src/components/create-organization.tsx
@@ -1,3 +1,6 @@
+import { useRef, type SubmitEvent } from "react"
+import axios from "axios";
+
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -13,6 +16,22 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export function CreateOrganizationModal({ open, onOpenChange }: { open: boolean, onOpenChange: (open: boolean) => void }) {
+  const organizationName = useRef<HTMLInputElement>({} as HTMLInputElement);
+
+  const onSubmit = async (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newOrganizationName = organizationName.current.value;
+
+    if (!newOrganizationName) return;
+
+    try {
+      await axios.post("http://localhost:3001/organizations", { name: newOrganizationName });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Dialog open={open}>
       <DialogContent className="sm:max-w-sm p-2" showCloseButton={false}>
@@ -22,20 +41,24 @@ export function CreateOrganizationModal({ open, onOpenChange }: { open: boolean,
             Crie uma nova organização para gerenciar feedback dos projetos
           </DialogDescription>
         </DialogHeader>
+        <form onSubmit={onSubmit}>
           <Field className="px-4">
             <Label htmlFor="name-1">Nome da organização</Label>
-            <Input id="name-1" name="name" />
+            <Input id="name-1" name="name" ref={organizationName} />
           </Field>
-        <DialogFooter className="flex flex-row items-center py-8">
-          <DialogClose
-            render={
-              <Button onClick={() => onOpenChange(false)} variant="secondary" className="flex-1 h-12 hover:cursor-pointer transition-all duration-300">
-                Cancel
-              </Button>
-            }
-          />
-          <Button type="submit" className="flex-1 h-12 hover:cursor-pointer transition-all duration-300">Save changes</Button>
-        </DialogFooter>
+          <DialogFooter className="flex flex-row items-center py-8">
+            <DialogClose
+              render={
+                <Button onClick={() => onOpenChange(false)} variant="secondary" className="flex-1 h-12 hover:cursor-pointer transition-all duration-300">
+                  Cancel
+                </Button>
+              }
+            />
+            <Button type="submit" className="flex-1 h-12 hover:cursor-pointer transition-all duration-300">
+              Save changes
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
