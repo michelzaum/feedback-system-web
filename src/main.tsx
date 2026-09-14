e3ca96c8ef208e0e620a20db658e4ff041ee73cf
michelzaum <michel.oliveira999@hotmail.com>
2026-09-14 12:27:21 -0300

    chore: install @clerk/react

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router";
+import { ClerkProvider } from '@clerk/react';
 
-import './index.css'
+import './index.css';
 import { RoutesComponent } from './routes.tsx';
 import App from './App.tsx';
 import { Toaster } from './components/ui/sonner.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App>
-        <RoutesComponent />
-        <Toaster richColors />
-      </App>
-    </BrowserRouter>,
+    <ClerkProvider>
+      <BrowserRouter>
+        <App>
+          <RoutesComponent />
+          <Toaster richColors />
+        </App>
+      </BrowserRouter>,
+    </ClerkProvider>
   </StrictMode>,
 )
