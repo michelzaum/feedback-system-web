d17bf665148d03b689ce7d949298de9c1ca76e26
michelzaum <michel.oliveira999@hotmail.com>
2026-09-14 20:08:54 -0300

    Revert "feat: redirect to sign-in page when not authenticated"
    This reverts commit ad3195038f232c26fed1cef4348a544578793dc9.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, useLocation } from "react-router";
+import { BrowserRouter } from "react-router";
 import { ClerkProvider } from '@clerk/react';
 
 import './index.css';
@@ -8,20 +8,14 @@ import { RoutesComponent } from './routes.tsx';
 import App from './App.tsx';
 import { Toaster } from './components/ui/sonner.tsx';
 
-function LayoutWrapper({ children }: { children: React.ReactNode }) {
-  const location = useLocation();
-  const isAuthPage = location.pathname === "/sign-in" || location.pathname === "/sign-up";
-  return isAuthPage ? <>{children}</> : <App>{children}</App>;
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider>
       <BrowserRouter>
-        <LayoutWrapper>
+        <App>
           <RoutesComponent />
           <Toaster richColors />
-        </LayoutWrapper>
+        </App>
       </BrowserRouter>,
     </ClerkProvider>
   </StrictMode>,
